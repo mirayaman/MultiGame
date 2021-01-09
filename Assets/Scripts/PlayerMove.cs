b0db72bdfd90a597ec045f6d20a6a7c340ea61c8
Mira Ymana <77152215+mirayaman@users.noreply.github.com>
2021-01-09 18:07:53 +0900

    Update PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField, Range(1.0f, 10.0f)] private float speed = 1.5f; //Скорость перемещения персонажа
-    [SerializeField, Range(1.0f, 10.0f)] private float JumpForce = 5.0f; //Сила толчка для прыэка
+    [SerializeField, Range(1.0f, 10.0f)] private float JumpForce = 5.0f; //Сила придаваемая для прыжка
     [SerializeField, Range(0.2f, 10.0f)] private float jumpDistance = 1.2f; //Минимальная дистанция для прыжка
 
     [Range(1.0f, 100.0f)] public float sensetivity = 5.0f; //Чувствительность мыши игрока
@@ -21,7 +21,7 @@ public class PlayerMove : MonoBehaviour
     {
         RaycastHit hit; //Переменная столкновений
         Ray ray = new Ray(transform.position + new Vector3(0, 1), Vector3.down); //Луч пущеный из центра игрока
-        if(Physics.Raycast(ray, out hit, jumpDistance)) //Передаём данные из луча в переменную hit. Если есть столкновение и обьект находиться на дистанции меньшой или равной jumpDistance возврощаем true
+        if(Physics.Raycast(ray, out hit, jumpDistance)) //Передаём данные из луча в переменную hit. Если есть столкновение и обьект находиться на дистанции меньшей или равной jumpDistance возврощаем true
         {
             return true;
         }
@@ -34,7 +34,7 @@ public class PlayerMove : MonoBehaviour
         rotationY += mouseY * sensetivity * Time.deltaTime; //Меняем значение положения в пространстве по оси y
         rotationY = Mathf.Clamp(rotationY, minYrotation, maxYrotation); //Не допускаем что-бы позиция была выше/ниже наших порогов
 
-        camera.localEulerAngles = new Vector3(-rotationY, rotationX);//Меняем  положениу в пространстве
+        camera.localEulerAngles = new Vector3(-rotationY, rotationX);//Меняем  положение в пространстве
     }
 
     private void ControlePlayerMove(float horizontal = 0.0f, float vertical = 0.0f)//Метод перемещения игрока. Принимает в себя wasd или же стик джойстика
@@ -56,7 +56,7 @@ public class PlayerMove : MonoBehaviour
         moveDerect = camera.TransformDirection(moveDerect);
         moveDerect = new Vector3(moveDerect.x, 0.0f, moveDerect.z);
 
-        if(isGround() && Input.GetKey(KeyCode.Space))//Проверяем землю и нажатие клавиши прыжка!
+        if(isGround() && Input.GetKey(KeyCode.Space))//Проверяем землю (Объект игрок находится на земле) и нажатие клавиши прыжка!
         {
             rigidbody.velocity = new Vector2(0, JumpForce);//Прыгаем.
         }
@@ -78,7 +78,7 @@ public class PlayerMove : MonoBehaviour
 
 		if(camera == null)
         {
-            camera = transform.Find("CameraBox"); camera = camera.Find("PlayerCamera"); //Это кастыль. Так делать плохо, но иногда можно.
+            camera = transform.Find("CameraBox"); camera = camera.Find("PlayerCamera"); //Это костыль. Так делать плохо, но иногда можно.
         }
         
     }
