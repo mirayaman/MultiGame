ced91cbda4011c5b873304d83801d1b169f92fad
Mira Ymana <77152215+mirayaman@users.noreply.github.com>
2021-01-16 23:07:17 +0900

    Новая версия проекта и по этому новая ветка!
    В данной ветке был изменён 1 скрипт и добавлено 3 новых скрипта. А также была перенастроена сцена. Для каждого ролика будет создаваться новая ветка.

---
+++ Scripts/ActionPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMove))]
+public class ActionPlayer : MonoBehaviour
+{
+    private PlayerMove motorPlayer = null;
+
+    private void Start()
+    {
+        motorPlayer = GetComponent<PlayerMove>();
+        LoadSettings();
+    }
+
+
+    private void FixedUpdate()
+    {
+        motorPlayer.Tick();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LoadSettings() 
+    {
+        float getingFloat = PlayerPrefs.GetFloat("sensetivity", 50.0f);
+        motorPlayer.sensetivity = getingFloat;
+
+        string getingKey = PlayerPrefs.GetString("keyJump", "Space");
+        motorPlayer.keyJump = FindKey(getingKey);
+    }
+
+    private KeyCode FindKey(string name = "None")
+    {
+        foreach(KeyCode soughtKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (soughtKey.ToString() == name) return soughtKey;
+        }
+
+        return KeyCode.None;
+    }
+
+}
---
+++ Scripts/SpecificationsPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(DrawingUI))]
+public class SpecificationsPlayer : MonoBehaviour
+{
+    [SerializeField] private int[] values = { 100, 100, 0, 0 };
+    /*
+     * id | name value
+     *  0 | Heat Point
+     *  1 | Have gold
+     *  2 | Have iron
+     *  3 | Have bullet
+     */
+    [SerializeField] private DrawingUI drawUI = null;
+
+    private void Start()
+    {
+        drawUI = GetComponent<DrawingUI>();
+    }
+
+    public void changValue(int id, int value)
+    {
+        values[id] = values[id] + value;
+        drawUI.UpdateStatusBar(values);
+    }
+
+}
